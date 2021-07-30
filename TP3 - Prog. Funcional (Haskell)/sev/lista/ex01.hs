7293e820af5992850b727aad5e1c3b1ba32fc9f1
Arthur Severo <svzr.47@gmail.com>
2021-07-30 18:44:30 -0300

    aprendi a usar a main

--- TP3 - Prog. Funcional (Haskell)/sev/lista/ex01.hs
+++ TP3 - Prog. Funcional (Haskell)/sev/lista/ex01.hs
@@ -1,8 +1,11 @@
+module Main where
 list :: [Integer]
-list = [0, 1, 2, 3, 4, 5]
+list = [0, 2..]
 
 foundInteger :: Integer -> [Integer] -> Bool
 foundInteger x [] = False
 foundInteger x (y:l)
     | x == y = True
     | otherwise = foundInteger x l
+
+main = do print(foundInteger 500 list)
