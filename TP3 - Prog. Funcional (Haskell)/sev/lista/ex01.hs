a7229265664872fa5abdfde62bc38cce03490220
Arthur Severo <svzr.47@gmail.com>
2021-08-04 16:21:58 -0300

    fix ex01

--- TP3 - Prog. Funcional (Haskell)/sev/lista/ex01.hs
+++ TP3 - Prog. Funcional (Haskell)/sev/lista/ex01.hs
@@ -1,6 +1,6 @@
 module Main where
 list :: [Integer]
-list = [0, 2..]
+list = [0, 2, 4, 6, 8, 10]
 
 foundInteger :: Integer -> [Integer] -> Bool
 foundInteger x [] = False
@@ -8,4 +8,4 @@ foundInteger x (y:l)
     | x == y = True
     | otherwise = foundInteger x l
 
-main = do print(foundInteger 500 list)
+main = do print(foundInteger 1 list)
